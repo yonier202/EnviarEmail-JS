b3eca89e4ef01aaa1aa514edcac11bd957707dde
JhonierRojas <yonier202@gmail.com>
2024-02-16 22:17:09 -0500

    reset, submit, spinner y mensaje de envio

--- js/app.js
+++ js/app.js
@@ -13,11 +13,48 @@ document.addEventListener("DOMContentLoaded", function(){
     const inputMensaje=document.querySelector('#mensaje');
     const Formulario=document.querySelector('#formulario');
     const btnSubmit=document.querySelector('#formulario button[type="submit"]');
+    const btnReset=document.querySelector('#formulario button[type="reset"]');
+    const spinner=document.querySelector('#spinner');
+
 
     inputEmail.addEventListener("blur", validar);
     inputAsunto.addEventListener("blur", validar);
     inputMensaje.addEventListener("blur", validar);
 
+    Formulario.addEventListener("submit", enviarEmail);
+    
+    btnReset.addEventListener("click", function(e) {
+        e.preventDefault();
+        resetFormulario();
+    });
+
+
+    function enviarEmail(e){
+        e.preventDefault();
+
+        spinner.classList.add("flex");
+        spinner.classList.remove("hidden");
+
+        setTimeout(() => {
+                spinner.classList.remove("flex");
+                spinner.classList.add("hidden");
+                resetFormulario();
+        }, 3000);
+
+        //crear una alerta
+
+        const alertaExito = document.createElement("P");
+        alertaExito.classList.add('bg-green-500', 'text-white', 'p-2', 'text-center',
+        'rounded-lg', 'mt-10', 'font-bold', 'text-sm', 'uppercase');
+        alertaExito.textContent = 'Mensaje Enviado Correctamente';
+
+        Formulario.appendChild(alertaExito);
+
+        setTimeout(() => {
+            alertaExito.remove();
+        }, 3000);
+
+    }
     function validar(e) {
         if (e.target.value.trim()==="") {
             mostrarAlerta(`el campo ${e.target.id} es obligatorio`, e.target.parentElement);
@@ -78,6 +115,16 @@ document.addEventListener("DOMContentLoaded", function(){
             btnSubmit.classList.remove('opacity-50');
             btnSubmit.disabled = false;
         }
+    }
+
+    function resetFormulario(){
+        Formulario.reset();
+
+        content.email = "";
+        content.asunto = "";
+        content.mensaje = "";
+
+        comprobarInputs();
     }
 
 });
